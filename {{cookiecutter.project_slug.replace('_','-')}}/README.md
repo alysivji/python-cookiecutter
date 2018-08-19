7a65a7863dd5f1bece333c87b09909fd9e98bd9e
Aly Sivji <alysivji@gmail.com>
2018-08-19 09:45:34 -0500

    Update with lessons learned

--- {{cookiecutter.project_slug.replace('_','-')}}/README.md
+++ {{cookiecutter.project_slug.replace('_','-')}}/README.md
@@ -1,3 +1,5 @@
 # {{ cookiecutter.project_name }}
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 {{ cookiecutter.project_description }}
--- {{cookiecutter.project_slug.replace('_','-')}}/setup.py
+++ {{cookiecutter.project_slug.replace('_','-')}}/setup.py
@@ -1,16 +1,54 @@
+import codecs
+import os
+import re
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
     name='{{ cookiecutter.project_slug.replace('_','-') }}',
-    version='0.0.1',
-    description='{{ cookiecutter.project_description }}',
-    url='https://github.com/{{ cookiecutter.github_id }}/{{ cookiecutter.project_slug.replace('_','-') }}',
+    version=find_version("falcon_apispec", "_version.py"),
+
     author='{{ cookiecutter.author }}',
     author_email='{{ cookiecutter.author_email }}',
+
+    description='{{ cookiecutter.project_description }}',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/{{ cookiecutter.github_id }}/{{ cookiecutter.project_slug.replace('_','-') }}',
+
     classifiers=[
-        'Programming Language :: Python :: 3.6',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages(exclude=['tests', ]),
+    keywords="",
+
     install_requires=[''],
-    download_url='https://github.com/{{ cookiecutter.github_id }}/{{ cookiecutter.project_slug.replace('_','-') }}',
+    packages=find_packages(exclude=['tests', ]),
+    test_suite='tests',
+
+    zip_safe=False,
 )
